dd618faa45ad25834c0b281e3b2ccde8fe534d92
Mikhail Swift <mswift@novetta.com>
2020-02-17 14:41:48 -0500

    polybar updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar main &
+DEFAULT_NETWORK_IFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1) polybar main &

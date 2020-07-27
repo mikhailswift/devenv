1597515f10b7a4fc3d1bea3deca1e19fd2ff8e23
Mikhail Swift <mswift@mswift.dev>
2020-07-27 17:07:25 -0400

    Updates to config for better multimonitor support
    * Switch to chromium in dunstrc
    * Add move workspace keybind for i3
    * Show polybar on each active monitor in xrandr
    * Add playerctl to installed packages

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-DEFAULT_NETWORK_IFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1) polybar main &
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        DEFAULT_NETWORK_IFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1) MONITOR=$m polybar --reload main &
+    done
+else
+    DEFAULT_NETWORK_IFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1) polybar main &
+fi
